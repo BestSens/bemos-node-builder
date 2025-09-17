5dae030ae73055386be53e85c8ccf9f6b1f0152a
Jan Schöppach <jan.schoeppach@metax-gmbh.de>
2025-09-17 13:28:09 +0200

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM fedora:42
+
+RUN dnf install -y \
+        git \
+        curl \
+        wcurl \
+        g++ \
+        gcc \
+        make \
+        patch \
+        ninja-build \
+        python3 \
+        ccache \
+        libgcc.i686 \
+        libstdc++-devel \
+        libstdc++-devel.i686 \
+        libstdc++-static.i686 \
+        glibc-devel \
+        glibc-devel.i686 \
+        glibc-static.i686 \
+        libatomic-static.i686 && \
+    dnf clean all && \
+    rm -rf /var/cache/dnf
+
+ADD https://github.com/BestSens/musl-build-image/releases/download/v1.3.7/arm-bemos-linux-musleabihf_1.3.7.tar.gz /
+ENV PATH="/opt/x-tools/arm-bemos-linux-musleabihf/bin:$PATH"
+
+ENV CCACHE_DIR="/ccache"
+
+COPY run.sh /work/run.sh
+COPY fix_ninja_build.patch /work/fix_ninja_build.patch
+
+VOLUME /ccache
+
+ENTRYPOINT [ "/work/run.sh" ]
---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+export CC="ccache arm-bemos-linux-musleabihf-gcc -march=armv7-a -mfloat-abi=hard -mtune=cortex-a9 -flax-vector-conversions"
+export CXX="ccache arm-bemos-linux-musleabihf-g++ -march=armv7-a -mfloat-abi=hard -mtune=cortex-a9"
+export CC_host="ccache gcc -m32"
+export CXX_host="ccache g++ -m32"
+
+cd /work
+
+wcurl https://nodejs.org/dist/$1/node-$1.tar.gz || exit 1
+tar -xf node-$1.tar.gz
+cd node-$1
+patch -p1 < ../fix_ninja_build.patch || exit 1
+
+./configure --ninja --prefix=./dist --dest-cpu=arm --cross-compiling --fully-static --dest-os=linux --with-arm-float-abi=hard
+
+ninja -C out/Release node
+
+mkdir -p archive/node-${1}/bin
+cp out/Release/node archive/node-${1}/bin
+cp LICENSE archive/node-${1}/
+
+tmp_archive="$(mktemp /tmp/node-${1}-out-XXXXXX.tar.gz)" || exit 1
+tar -C archive -czf "$tmp_archive" . || exit 1
+mv "$tmp_archive" /out/ || exit 1
+chmod 644 /out/node-${1}-out-*.tar.gz
